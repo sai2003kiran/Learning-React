f6d262f8704877eddfc547d0ac9877a599e2dabd
saikiran <saik03414@gmail.com>
2026-01-12 14:55:58 +0530

    useState() is a React Hook used to manage state in functional components and trigger re-rendering when the state changes.

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,18 +1,36 @@
+import { useState } from "react";
 import { restaurantData } from "../Utility/Constant";
 import { Restaurant } from "./Restaurant";
 
+
 export var Body = () => {
+    //state variable to hold the data
+    const [listOfRestaurants, setListOfRestaurants] = useState(restaurantData);
     return (
         <div className="Body">
             <div className="search">
                 <input type="text" placeholder="Search for restaurants, cuisines or a dish" />
                 <button>Search</button>
             </div>
+
+            <div className="filter">
+                <button
+                    onClick={() => {
+                        const filteredList = listOfRestaurants.filter(
+                            (res) => res.card.card.info.avgRating > 4.5
+                        );
+                        setListOfRestaurants(filteredList);
+                    }}
+                >
+                    Top Rated Restaurants
+                </button>
+            </div>
+
             <div className="restaurant-list">
-                {restaurantData.map((restaurant) => (
+                {listOfRestaurants.map((restaurant) => (
                     <Restaurant key={restaurant.card.card.info.id} restObj={restaurant} />
                 ))}
-        </div>
+            </div>
         </div>
     );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 export var Header = () => {
+    const [button, setButton] = useState("login");
     return (
         <div className="Outerheader">
             <div className="logo">
-                <img src="https://img.freepik.com/free-vector/delivery-logo-with-bike-man-courier_1308-49806.jpg?semt=ais_hybrid&w=740&q=80" style={{ width: "150px" }}></img>
+                <img src="https://img.freepik.com/free-vector/delivery-logo-with-bike-man-courier_1308-49806.jpg?semt=ais_hybrid&w=740&q=80" style={{ width: "100px" }}></img>
             </div>
             <div className="nav-items">
                 <ul>
@@ -10,6 +12,20 @@ export var Header = () => {
                     <li>About Us</li>
                     <li>Contact</li>
                     <li>Cart</li>
+                    <li>
+                        <button
+                            onClick={() => {
+                                if (button == "login") {
+                                    setButton("logout");
+                                } else {
+                                    setButton("login");
+                                }
+                            }}
+                            className="login-button s"
+                        >
+                            {button}
+                        </button>
+                    </li>
                 </ul>
             </div>
         </div>
